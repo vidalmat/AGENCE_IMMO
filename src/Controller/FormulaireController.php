75f667b20f842d880e2b2d0470eede9a83b41703
Matthias <vidalmat06@gmail.com>
2021-03-31 16:22:51 +0200

    Essaie ajout form sur formulaire

--- src/Controller/FormulaireController.php
+++ src/Controller/FormulaireController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
 
 class FormulaireController extends AbstractController
 {
@@ -15,6 +18,29 @@ class FormulaireController extends AbstractController
     {
         return $this->render('formulaire/index.html.twig', [
             'controller_name' => 'FormulaireController',
+        ]);
+    }
+
+    /**
+     * @route(/formulaire/newForm, name="formulaire")
+     */
+    public function newForm(Request $request, ObjectManager $manager){
+
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+                     ->add("nom")
+                     ->add("prenom")
+                     ->add("adresse")
+                     ->add("tel")
+                     ->add("email")
+                     ->add("password")
+                     ->getForm();
+
+
+
+        return $this->render("formulaire/index.html.twig", [
+            "formUser" => $form->createView()
         ]);
     }
 }
