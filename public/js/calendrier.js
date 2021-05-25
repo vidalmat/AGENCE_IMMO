1ebe1c00b89ed188bcdfac9f9a67b91c0b254a39
Matthias <vidalmat06@gmail.com>
2021-05-25 09:30:28 +0200

    modif gitignore

---
+++ public/js/calendrier.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendrier');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      locale:"fr",
+      selectable: true,
+      timeZone:"Europe/Paris",
+      headerToolbar: {
+        start: "prev,next today",
+        center: "title",
+        end:"dayGridMonth,timeGridWeek,timeGridDay"
+  // window.onload = () => {
+  //   let calendarElt = document.querySelector("#calendrier")
+  //   let calendrier = new FullCalendar.Calendrier(calendarElt, {
+  //     initialView: "timeGridWeek",
+  //     locale: "fr",
+  //     timeZone: "Europe/Paris",
+  //     headerToolbar: {
+  //       start: "prev,next,today",
+  //       center: "title",
+  //       end: "dayGridMonth, timeGridWeek, timeGridDay"
+  //     }
+  //   })
+  },
+      //events: {{ data|raw }}
+       events: '/datas/rdvs.json'
+       /*{
+         url: Routing.generate('rdv/index.html.twig', { nom: $('data').attr('id')}),}*/,
+      }
+      );
+      calendar.render();
+    
+  });
+
+FullCalendar.globalLocales.push(function () {
+'use strict';
+
+var fr = {
+code: 'fr',
+week: {
+  dow: 1, // Monday is the first day of the week.
+  doy: 4, // The week that contains Jan 4th is the first week of the year.
+},
+buttonText: {
+  prev: 'Précédent',
+  next: 'Suivant',
+  today: "Aujourd'hui",
+  year: 'Année',
+  month: 'Mois',
+  week: 'Semaine',
+  day: 'Jour',
+  list: 'Planning',
+},
+weekText: 'Sem.',
+allDayText: 'Toute la journée',
+moreLinkText: 'en plus',
+noEventsText: 'Aucun événement à afficher',
+};
+
+return fr;
+
+}());
